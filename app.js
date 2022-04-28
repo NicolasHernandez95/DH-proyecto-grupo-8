e6e7f82f3298dbb4d83441b27446ba5232702e8a
EzequielDuartes <ddezequiel@hotmail.com>
2022-04-28 11:22:46 -0300

    PI8

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+//agrego ruta a productDetail
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
